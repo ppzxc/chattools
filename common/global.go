4f99fda56edfed37cbf0e774fc91bd1396da0308
JeongHa-Cho <cjh8487@naver.com>
2021-11-05 11:10:01 +0900

    fix(mongodb): add time out second variable

--- common/global.go
+++ common/global.go
@@ -1,5 +1,12 @@
 package common
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"time"
+)
 
 var Validate *validator.Validate
+
+var (
+	WriteTimeOut = 5 * time.Second
+)
