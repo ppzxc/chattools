ae29c54ad43a85cb34409a0958a4118a455009e7
JeongHa-Cho <cjh8487@naver.com>
2021-11-11 16:26:04 +0900

    fix(domain): pub sub protocol fix

--- domain/domain.go
+++ domain/domain.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/ppzxc/chattools/domain/outbound"
+import (
+	"github.com/ppzxc/chattools/domain/outbound"
+	"github.com/sirupsen/logrus"
+)
 
 type Command int
 
@@ -17,10 +20,12 @@ const (
 	NotifyCrud
 )
 
-type SyncProtocol struct {
-	Command       Command       `json:"command"`
-	TransactionId string        `json:"transaction_id"`
-	UserId        int64         `json:"user_id"`
-	TopicId       int64         `json:"topic_id"`
-	Payload       outbound.Root `json:"payload"`
+type PubSubProtocol struct {
+	Command Command       `json:"command"`
+	TopicId int64         `json:"topic_id"`
+	Payload outbound.Root `json:"payload"`
+	Fields  logrus.Fields `json:"fields"`
+
+	//TransactionId string        `json:"transaction_id"`
+	//UserId        int64         `json:"user_id"`
 }
