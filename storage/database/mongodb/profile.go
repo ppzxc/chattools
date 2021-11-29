7849283b7cf0b6f67b8a8f867ad99ab5eaf7c181
JeongHa-Cho <cjh8487@naver.com>
2021-11-29 15:29:05 +0900

    fix(profile): change to meta

--- storage/database/mongodb/profile.go
+++ storage/database/mongodb/profile.go
@@ -3,11 +3,25 @@ package mongodb
 import (
 	"context"
 	"github.com/ppzxc/chattools/storage/database"
-	model2 "github.com/ppzxc/chattools/storage/database/model"
+	"github.com/ppzxc/chattools/storage/database/model"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (m mongodb) ProfileImageUpdate(ctx context.Context, file model2.File) (int64, error) {
+func (m mongodb) ProfileImageFind(ctx context.Context, userId int64) (model.File, error) {
+	filter, err := m.crudUser.FindOneByFilter(ctx, bson.D{{"_id", userId}})
+	if err != nil {
+		return model.File{}, err
+	}
+
+	if filter.Profile.FileId > 0 {
+		return m.crudFile.FindOneByFilter(ctx, bson.D{{"_id", filter.Profile.FileId}})
+	} else {
+		return model.File{}, mongo.ErrNoDocuments
+	}
+}
+
+func (m mongodb) ProfileImageUpdate(ctx context.Context, file model.File) (int64, error) {
 	sequence, err := m.crudSeq.Next(ctx, database.MongoCollectionFile)
 	if err != nil {
 		return 0, err
@@ -32,7 +46,7 @@ func (m mongodb) ProfileImageUpdate(ctx context.Context, file model2.File) (int6
 	return file.Id, nil
 }
 
-func (m mongodb) ProfileUpdateByUserId(ctx context.Context, profile model2.Profile) error {
+func (m mongodb) ProfileUpdateByUserId(ctx context.Context, profile model.Profile) error {
 	return m.crudUser.FindOneAndUpdateByFilter(
 		ctx,
 		bson.D{{"_id", profile.UserId}},
