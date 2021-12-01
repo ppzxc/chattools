bebabca815f37373ff388e89899122eaf7720efe
JeongHa-Cho <cjh8487@naver.com>
2021-12-01 14:27:35 +0900

    fix(mongo): add options

--- storage/database/model/paging.go
+++ storage/database/model/paging.go
@@ -6,4 +6,6 @@ type Paging struct {
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 	Id        int64      `json:"id,omitempty"`
+	Offset    int64      `json:"offset,omitempty"`
+	Limit     int64      `json:"limit,omitempty"`
 }
